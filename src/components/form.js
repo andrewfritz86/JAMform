7e85732f1f17e0d4500c718ec643b63346ccffd0
Andrew Fritz <andrew.fritz86@gmail.com>
2020-03-29 14:24:45 -0400

    Add form reducer and handler

--- src/components/form.js
+++ src/components/form.js
@@ -1,32 +1,88 @@
-import React from "react";
+import React, { useReducer } from "react";
 import styles from "./form.module.css";
 
-const handleSubmit = e => {
-  e.preventDefault();
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  subject: "",
+  body: ""
 };
 
-const form = () => {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "updateFieldValue":
+      return { ...state, [action.field]: action.value };
+
+    default:
+      return INITIAL_STATE;
+  }
+};
+
+const Form = () => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  // Curried function below,
+  // Instead of simply calling a function with 2 args, call the function with one arg, then return another function that
+  // Accepts the *SECOND* argument
+  // Allows a function to be returned that has the scope of all the outer scope variables
+  // This is tricky to see in use because the second arg (e) comes for free
+  // onChange={e => updateFieldValue("name")(e)} is more illustrative
+  const updateFieldValue = field => event => {
+    dispatch({
+      type: "updateFieldValue",
+      field,
+      value: event.target.value
+    });
+  };
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <label className={styles.label}>
         name
-        <input className={styles.input} type="text" name="name" />
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={e => updateFieldValue("name")(e)}
+        />
       </label>
       <label className={styles.label}>
         email
-        <input className={styles.input} type="email" name="email" />
+        <input
+          className={styles.input}
+          type="email"
+          name="email"
+          value={state.email}
+          onChange={updateFieldValue("email")}
+        />
       </label>
       <label className={styles.label}>
         subject
-        <input className={styles.input} type="text" name="subject" />
+        <input
+          className={styles.input}
+          type="text"
+          name="subject"
+          value={state.subject}
+          onChange={updateFieldValue("subject")}
+        />
       </label>
       <label className={styles.label}>
         Body
-        <textarea className={styles.input} name="body" />
+        <textarea
+          className={styles.input}
+          name="body"
+          value={state.body}
+          onChange={updateFieldValue("body")}
+        />
       </label>
       <button className={styles.button}> send </button>
     </form>
   );
 };
 
-export default form;
+export default Form;
